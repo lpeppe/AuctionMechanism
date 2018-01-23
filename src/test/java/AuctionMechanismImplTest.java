72d9b85575ff7158d1f86d52cc339775a050de53
mariaelena <m.cammarano25@studenti.unisa.it>
2018-01-23 10:28:59 +0100

    Unit Testing bug fixed

--- src/test/java/AuctionMechanismImplTest.java
+++ src/test/java/AuctionMechanismImplTest.java
@@ -1,7 +1,5 @@
 import net.tomp2p.peers.Number160;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -10,31 +8,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AuctionMechanismImplTest {
 
-    private AuctionMechanismImpl peer0;
-    private AuctionMechanismImpl peer1;
-    private AuctionMechanismImpl peer2;
+    private static AuctionMechanismImpl peer0;
+    private static AuctionMechanismImpl peer1;
+    private static AuctionMechanismImpl peer2;
 
-    @BeforeEach
-    void setup() throws Exception{
+    @BeforeAll
+    static void setup() throws Exception{
         peer0 = new AuctionMechanismImpl(0);
         peer1 = new AuctionMechanismImpl(1);
         peer2 = new AuctionMechanismImpl(2);
     }
 
-    @AfterEach
-    void tearDown() {
+    @AfterAll
+    static void tearDown() {
         peer0.leave();
         peer1.leave();
         peer2.leave();
     }
 
-    @Test
+     @Test
     void createDuplicateAuction() {
         try {
-            //AuctionMechanismImpl peer0 = new AuctionMechanismImpl(0);
             assertTrue(peer0.createAuction("TV", new Date(Calendar.getInstance().getTimeInMillis() + 1000), 1000, "LG OLED 4K"));
             assertFalse(peer0.createAuction("TV", new Date(Calendar.getInstance().getTimeInMillis() + 1000), 1000, "LG OLED 4K"));
-            //assertTrue(peer0.leave());
         }
         catch(Exception e) {
             e.printStackTrace();
@@ -44,58 +40,30 @@ class AuctionMechanismImplTest {
     @Test
     void checkAuctionNoBids() {
         try {
-            //AuctionMechanismImpl peer0 = new AuctionMechanismImpl(0);
-            peer0.createAuction("TV", new Date(Calendar.getInstance().getTimeInMillis() + 1000), 1000, "LG OLED 4K");
-            assertEquals(peer0.checkAuction("TV"), "status: open\nbids: ");
+
+            peer0.createAuction("TV2", new Date(Calendar.getInstance().getTimeInMillis() + 1000), 1000, "LG OLED 4K");
+            assertEquals(peer0.checkAuction("TV2"), "status: open\nbids: ");
             Thread.sleep(1000);
-            assertEquals(peer0.checkAuction("TV"), "status: ended\nwinner: none\n");
-            //assertTrue(peer0.leave());
+            assertEquals(peer0.checkAuction("TV2"), "status: ended\nwinner: none\n");
         }
         catch(Exception e) {
             e.printStackTrace();
         }
     }
 
-    @Test
-    void checkAuctionMultipleBids() {
+   @Test
+    void checkMultipleAuctions() {
         try {
-            //AuctionMechanismImpl peer0 = new AuctionMechanismImpl(0);
-            //AuctionMechanismImpl peer1 = new AuctionMechanismImpl(1);
-            //AuctionMechanismImpl peer2 = new AuctionMechanismImpl(2);
-            peer0.createAuction("TV", new Date(Calendar.getInstance().getTimeInMillis() + 1000), 1000, "LG OLED 4K");
-            peer1.placeAbid("TV", 2000);
-            peer2.placeAbid("TV", 1500);
-            assertEquals(peer0.checkAuction("TV"), "status: open\nbids: [Peer 1: 2000.0] [Peer 2: 1500.0] ");
-            Thread.sleep(1000);
-            assertEquals(peer0.checkAuction("TV"), "status: ended\nwinner: Peer 1\nprice: 1500.0\n");
-            //assertTrue(peer0.leave());
-            //assertTrue(peer1.leave());
-            //assertTrue(peer2.leave());
-        }
-        catch(Exception e) {
-            e.printStackTrace();
-        }
-    }
+            peer0.createAuction("Persiana", new Date(Calendar.getInstance().getTimeInMillis() + 1000), 1000, "cdedf");
+            //peer1.createAuction("Auto", new Date(Calendar.getInstance().getTimeInMillis() + 1000), 28000, "Abarth 124 spyder");
+            //peer1.createAuction("Lavatrice", new Date(Calendar.getInstance().getTimeInMillis() + 1000), 200, "Lavatrice Bosch");
+            peer1.placeAbid("Persiana", 2000);
+            assertEquals(peer0.checkAuction("Persiana"), "status: open\nbids: [Peer 1: 2000.0] ");
+            peer2.placeAbid("Persiana", 1500);
+            assertEquals(peer0.checkAuction("Persiana"), "status: open\nbids: [Peer 1: 2000.0] [Peer 2: 1500.0] ");
+            Thread.sleep(2000);
+            assertEquals(peer0.checkAuction("Persiana"), "status: ended\nwinner: Peer 1\nprice: 1500.0\n");
 
-    @Test
-    void checkAuction() {
-        try {
-            //AuctionMechanismImpl peer0 = new AuctionMechanismImpl(0);
-            //AuctionMechanismImpl peer1 = new AuctionMechanismImpl(1);
-            //AuctionMechanismImpl peer2 = new AuctionMechanismImpl(2);
-            peer0.createAuction("TV", new Date(Calendar.getInstance().getTimeInMillis() + 1000), 1000, "LG OLED 4K");
-            peer1.createAuction("Auto", new Date(Calendar.getInstance().getTimeInMillis() + 1000), 28000, "Abarth 124 spyder");
-            peer1.createAuction("Lavatrice", new Date(Calendar.getInstance().getTimeInMillis() + 1000), 200, "Lavatrice Bosch");
-            assertEquals(peer0.checkAuction("TV"), "status: open\nbids: ");
-            peer1.placeAbid("TV", 2000);
-            assertEquals(peer0.checkAuction("TV"), "status: open\nbids: [Peer 1: 2000.0] ");
-            peer2.placeAbid("TV", 1500);
-            peer0.placeAbid("Auto", 5000);
-            Thread.sleep(1000);
-            assertEquals(peer0.checkAuction("TV"), "status: ended\nwinner: Peer 1\nprice: 1500.0\n");
-            //assertTrue(peer0.leave());
-            //assertTrue(peer1.leave());
-            //assertTrue(peer2.leave());
         }
         catch(Exception e) {
             e.printStackTrace();
